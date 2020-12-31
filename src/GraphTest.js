3dcf872c925371d11f772b857e001bed8e9046a3
Omar Shishani <omarshishanischool@gmail.com>
2020-12-31 15:57:10 -0800

    fix y-axis label with AM/PM helper function

--- src/GraphTest.js
+++ src/GraphTest.js
@@ -5,6 +5,18 @@ import { VictoryBar, VictoryChart, VictoryAxis, VictoryLine, VictoryTheme } from
 
 function c (msg, input) {return(console.log(`${msg}: `, input))};
 
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+
+
 class GraphTest extends React.Component {
   render() {
     const {dates, napStartTimes, napInfo} = this.props;
@@ -18,24 +30,25 @@ class GraphTest extends React.Component {
     let data;
     if (napInfo.length > 1) {
       data = napInfo.map((e, i) => {
+        c('napstarttime', e.napStartTime)
+        c('datetime', new Date(`January 1, 2000 ${e.napStartTime}`))
+        const dateTime = new Date(`January 1, 2000 ${e.napStartTime}`);
         const time = e.napStartTime.split(':');
         const timeNumber = Number(time[0]) + Number(time[1]/60);
-        c('timenum', timeNumber);
         const date = Math.floor(Date.parse(e.date)/1000/86400);
-        c('date', date);
         xAxisTickValues = [...xAxisTickValues, date];
         return(
-          { x: date, y: timeNumber }
+          { x: date, y: dateTime }
         );
       });
     }
-    console.log('ticks', xAxisTickValues);
     return (
       <>
         <div className="victory-chart-1-container">
           <VictoryChart
             theme={VictoryTheme.material}
             padding={{ left: 70, top: 20, right: 30, bottom: 50 }}
+            scale={{y:'time'}}
           >
             <VictoryAxis
               // tickValues specifies both the number of ticks and where
@@ -48,15 +61,12 @@ class GraphTest extends React.Component {
             <VictoryAxis
               dependentAxis
               // tickFormat specifies how ticks should be displayed
-              // tickFormat={(y) => (`${y}PM`)}
-              tickFormat={(y) => {
-                c('y',y)
-                c('y%1', y%1)
-                c('round', Math.round(y%1*10)/10)
-                return(
-                  `${y-y%1}:${Math.round(y%1*10)/10*60}PM`
-                );
-              }}
+              tickFormat={(y) => formatAMPM(y)}
+              // tickFormat={(y) => {
+              //   return(
+              //     `${y-y%1}:${Math.round(y%1*10)/10*60}PM`
+              //   );
+              // }}
             />
             <VictoryLine
               style={{
