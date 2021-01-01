27845055fd44186a1a6832aaa1401a964a071a8c
Omar Shishani <omarshishanischool@gmail.com>
2020-12-31 16:15:02 -0800

    Remove extra bar graph

--- src/GraphTest.js
+++ src/GraphTest.js
@@ -77,31 +77,6 @@ class GraphTest extends React.Component {
             />
           </VictoryChart>
         </div>
-
-
-
-        <VictoryChart
-          // domainPadding will add space to each side of VictoryBar to
-          // prevent it from overlapping the axis
-          domainPadding={20}
-        >
-          <VictoryAxis
-            // tickValues specifies both the number of ticks and where
-            // they are placed on the axis
-            tickValues={[1, 2, 3, 4]}
-            tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
-          />
-          <VictoryAxis
-            dependentAxis
-            // tickFormat specifies how ticks should be displayed
-            tickFormat={(x) => (`$${x / 1000}k`)}
-          />
-          <VictoryBar
-            data={data}
-            x="quarter"
-            y="earnings"
-          />
-        </VictoryChart>
       </>
     )
   }
